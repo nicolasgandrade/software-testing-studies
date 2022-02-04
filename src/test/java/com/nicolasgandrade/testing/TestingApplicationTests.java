c397d567dfd1d3d2b5747fe0362206e1e9098da2
nicolasgandrade <nicolasguerrero72@gmail.com>
2022-02-04 15:49:15 -0300

    basic unit test using JUnit

--- src/test/java/com/nicolasgandrade/testing/TestingApplicationTests.java
+++ src/test/java/com/nicolasgandrade/testing/TestingApplicationTests.java
@@ -3,11 +3,28 @@ package com.nicolasgandrade.testing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class TestingApplicationTests {
 
+	Calculator calcUnderTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void shouldAddNumbers() {
+		//Given
+		int a = 15;
+		int b = 20;
+
+		//When
+		int result = calcUnderTest.add(a, b);
+
+		//Then
+		int expected = 35;
+		assertThat(result).isEqualTo(expected);
+	}
+
+	class Calculator {
+		int add(int x, int y) {return x + y;}
 	}
 
 }
